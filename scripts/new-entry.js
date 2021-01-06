8ccb96e6b85663b8d8157bf0ab184753aaa17ed1
dtinth on devbox <dtinth@spacet.me>
2021-01-06 19:35:19 +0000

    Allow setting date when adding new entry (for archiving old posts from elsewhere)

--- scripts/new-entry.js
+++ scripts/new-entry.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
-const id = 
-  new Date()
+const id =
+  new Date(
+    process.argv[2] ? Date.parse(process.argv[2] + '+07:00') : Date.now()
+  )
     .toJSON()
-    .replace(/\W/g, "")
+    .replace(/\W/g, '')
     .slice(0, 15) +
-    "Z" +
-    (10000 + 10000 * Math.random()).toString().slice(-4)
+  'Z' +
+  (10000 + 10000 * Math.random()).toString().slice(-4)
 
 const path = 'data/' + id + '.md'
 fs.writeFileSync(path, '---\n---\n\n')
