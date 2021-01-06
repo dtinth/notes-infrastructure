37fea10d74791312dc71f90fcc03b3ddd793fa5a
dtinth on devbox <dtinth@spacet.me>
2021-01-06 19:36:13 +0000

    Increase preview page max age

--- .vscode/preview.js
+++ .vscode/preview.js
@@ -4,10 +4,13 @@ const secrets = require('../secrets')
 exports.execute = async (args) => {
   const vscode = args.require('vscode')
   try {
-    const id = vscode.window.activeTextEditor.document.fileName.split(/[/\\]/).pop().replace(/\.md$/, '')
+    const id = vscode.window.activeTextEditor.document.fileName
+      .split(/[/\\]/)
+      .pop()
+      .replace(/\.md$/, '')
     const jwt = jsonwebtoken.sign({ id }, secrets.previewSigningSecret, {
       algorithm: 'HS256',
-      expiresIn: 3600,
+      expiresIn: 86400,
     })
     vscode.env.openExternal(
       vscode.Uri.parse('https://notes.dt.in.th/preview-' + jwt)
